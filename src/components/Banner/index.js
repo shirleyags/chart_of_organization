e212448a75ba442c2ce38ff67870f9b3084ce17f
ShirleyAlice <shirleyags@yahoo.com.br>
2024-06-29 23:05:28 -0300

    Altera formatação

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,9 +1,12 @@
 import './Banner.css'
 
 const Banner = () => {
-  return(
+  return (
     <header className="banner">
-      <img src="/images/banner.png" alt="O banner principal da página do Organo"/>
+      <img
+        src="/images/banner.png"
+        alt="O banner principal da página do Organo"
+      />
     </header>
   )
 }
