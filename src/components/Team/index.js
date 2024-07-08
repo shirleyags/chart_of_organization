3c932fa4dda9e27cd527d1b5fc85a9c7d839466c
ShirleyAlice <shirleyags@yahoo.com.br>
2024-07-07 23:09:23 -0300

    Altera o style da section para hexToRgba e passa novas props

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,22 +1,26 @@
 import './Team.css'
 import Card from '../Card'
+import hexToRgba from 'hex-to-rgba'
 
-const Team = ({
-  name,
-  primaryColor,
-  secondaryColor,
-  employees,
-  whenDelete
-}) => {
+const Team = ({ name, color, employees, whenDelete, changeColor, id }) => {
   return (
     employees.length > 0 && (
-      <section className="team" style={{ backgroundColor: secondaryColor }}>
-        <h3 style={{ borderColor: primaryColor }}>{name}</h3>
+      <section
+        className="team"
+        style={{ backgroundColor: hexToRgba(color, '0.10') }}
+      >
+        <input
+          value={color}
+          type="color"
+          onChange={e => changeColor(e.target.value, id)}
+          className="input-color"
+        />
+        <h3 style={{ borderColor: color }}>{name}</h3>
         <div className="employees">
           {employees.map(employee => {
             return (
               <Card
-                cardBackgroundColor={primaryColor}
+                cardBackgroundColor={color}
                 key={employee.name}
                 name={employee.name}
                 ocupation={employee.ocupation}
